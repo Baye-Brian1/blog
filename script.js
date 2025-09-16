a96351ca85a3753ac5f64de724e0f0ead210f521
brian <bayebrian89@gmail.com>
2025-09-16 11:16:33 +0100

    finished upload functionalities

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 const URL='https://jsonplaceholder.typicode.com/posts';
-const blog= document.querySelector('.posts')
-const load= document.getElementById('load')
+const blog= document.querySelector('.posts');
+const load= document.getElementById('load');
+let currentStart = 0;
+const limit = 20;
  
 
 const displayBlog = async () => {
-  setTimeout(async ()=>{
-    const person = await fetch(URL);
-    const blogData= await person.json();
+  load.style.display = 'block';
+  setTimeout(async () => {
+    const response = await fetch(`${URL}?_start=${currentStart}&_limit=${limit}`);
+    const blogData = await response.json();
     showData(blogData);
-  },6000)
-  if (!showData) {
-    load.style.display = 'block';
-  } else {
     load.style.display = 'none';
-  }
+  }, 5000);
 };
 
 displayBlog();
-const showData= async (blogData) =>{
+
+const showData = (blogData) => {
   blogData.forEach(item => {
-    let blogg=document.createElement('div');
+    let blogg = document.createElement('div');
     blogg.classList.add('post');
-    blogg.innerHTML=`
-    <h3>${item.title}</h3>
-    <p>${item.body}</p>
-    <a href="blog.html?id=${item.id}">Read More...</a>`
+    blogg.innerHTML = `
+      <h3>${item.title}</h3>
+      <p>${item.body}</p>
+      <a href="blog.html?id=${item.id}">Read More...</a>`;
     blog.appendChild(blogg);
   });
-  
+  currentStart += limit;
 };
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    displayBlog();
+  }
+});
