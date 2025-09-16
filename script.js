930d3a2034edb976b9c7787456ef0a2627226632
brian <bayebrian89@gmail.com>
2025-09-16 10:40:20 +0100

    updated blog

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const URL='https://jsonplaceholder.typicode.com/posts';
 const blog= document.querySelector('.posts')
+const load= document.getElementById('load')
  
 const displayBlog = async () =>{
     const person= await fetch(URL);
      const blog= await person.json();
       showData(blog);
+      if (!showData) {
+        load.style.display='block';
+      } else {
+         load.style.display='none';
+      }
 };
 
 displayBlog();
