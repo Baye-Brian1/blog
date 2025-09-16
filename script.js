0041f2475882c063177d7b3f0e9d0698d0dd1bec
brian <bayebrian89@gmail.com>
2025-09-16 10:47:28 +0100

    loading update

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const URL='https://jsonplaceholder.typicode.com/posts';
 const blog= document.querySelector('.posts')
 const load= document.getElementById('load')
  
-const displayBlog = async () =>{
-    const person= await fetch(URL);
-     const blog= await person.json();
-      showData(blog);
-      if (!showData) {
-        load.style.display='block';
-      } else {
-         load.style.display='none';
-      }
+
+const displayBlog = async () => {
+  setTimeout(async ()=>{
+    const person = await fetch(URL);
+    const blogData= await person.json();
+    showData(blogData);
+  },6000)
+  if (!showData) {
+    load.style.display = 'block';
+  } else {
+    load.style.display = 'none';
+  }
 };
 
 displayBlog();
